f0f74ad3e1d498eb1749333a42389c39d2a32ff2
Sirajvind <99573323+sirajvind@users.noreply.github.com>
2025-07-18 01:06:52 +0800

    test: update Pulsifi Talent App smoke test to use placeholder selectors and test1@gmail.com
    - Switched to getByPlaceholder for email and password fields
    - Updated test email to test1@gmail.com
    - Cleaned up commented code for clarity

--- tests/e2e/ui-tests/pulsifi/talent-app.spec.ts
+++ tests/e2e/ui-tests/pulsifi/talent-app.spec.ts
@@ -4,13 +4,17 @@ const appUrl = 'https://staging-app.pulsifi.me';
 
 test.describe('smoke test for talent app', () => {
   test.beforeEach(async ({ page }) => {
-    test.setTimeout(120000);
     await page.goto(appUrl);
   });
 
   test('should load the app successfully', async ({ page }) => {
-    await page.getByRole('textbox', { name: 'Email' }).fill('test@gmail.com');
-    await page.getByRole('textbox', { name: 'Password' }).fill('Qa12345');
-    await page.getByRole('button', { name: 'Log In' }).click();
+    // await page.getByRole('textbox', { name: 'Email' }).fill('test@gmail.com');
+    // await page.getByRole('textbox', { name: 'Password' }).fill('Qa12345');
+    // await page.getByRole('button', { name: 'Log In' }).click();
+    
+    await page.getByPlaceholder('yours@example.com').fill('test1@gmail.com');
+    await page.getByPlaceholder('your password').fill('Qa12345');
+    await page.getByLabel('Log In').click();
+
   });
 });
