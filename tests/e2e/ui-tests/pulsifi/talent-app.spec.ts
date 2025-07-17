2728f1f0a748cc5a0db99aaa4f08b432d929a575
Sirajvind <99573323+sirajvind@users.noreply.github.com>
2025-07-18 02:12:14 +0800

    test: increase wait timeout to 65 seconds for Pulsifi Talent App smoke test

--- tests/e2e/ui-tests/pulsifi/talent-app.spec.ts
+++ tests/e2e/ui-tests/pulsifi/talent-app.spec.ts
@@ -6,10 +6,10 @@ test.describe('smoke test for talent app', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(appUrl);
     await page.waitForURL(/enterprise|id/, {waitUntil: 'load'});
+    await page.waitForTimeout(1 * 65 * 1000);
   });
 
   test('should load the app successfully', async ({ page }) => {
-    await page.waitForTimeout(30 * 1000);
     await page.getByRole('textbox', { name: 'Email' }).fill('test2@gmail.com');
     await page.getByRole('textbox', { name: 'Password' }).fill('Qa12345');
     await page.getByRole('button', { name: 'Log In' }).click();
